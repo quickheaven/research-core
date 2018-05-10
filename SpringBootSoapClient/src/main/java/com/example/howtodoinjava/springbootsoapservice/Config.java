7fab54d4fefa2a02876edf39ac9898f0d9b06781
quickheaven <arjie_c82@yahoo.com>
2018-05-09 22:50:16 -0400

    Spring Boot SOAP Web Service Client

---
+++ SpringBootSoapClient/src/main/java/com/example/howtodoinjava/springbootsoapservice/Config.java
@@ -0,0 +1,26 @@
+package com.example.howtodoinjava.springbootsoapservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+@Configuration
+public class Config {
+
+	@Bean
+	public Jaxb2Marshaller marshaller() {
+		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+		marshaller.setContextPath("com.example.howtodoinjava.schemas.school");
+		return marshaller;
+	}
+
+	@Bean
+	public SOAPConnector soapConnector(Jaxb2Marshaller marshaller) {
+		SOAPConnector client = new SOAPConnector();
+		client.setDefaultUri("");
+		client.setMarshaller(marshaller);
+		client.setUnmarshaller(marshaller);
+		return client;
+	}
+
+}
