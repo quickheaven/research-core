77f050b6edc602aa2327a097329c0500e750c2fa
Arjie Cristobal <ajie.io@yahoo.com>
2020-11-26 23:35:40 -0500

    Fork and Join

---
+++ fork-join-framework/src/main/java/ca/quickheaven/parallel/ForkJoinAdd.java
@@ -0,0 +1,4 @@
+package ca.quickheaven.parallel;
+
+public class ForkJoinAdd {
+}
---
+++ fork-join-framework/src/test/java/ca/quickheaven/parallel/ForkJoinFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.quickheaven.parallel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForkJoinFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
