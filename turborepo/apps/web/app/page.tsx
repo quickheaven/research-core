dfd15b9d5681061b1b8fb8cdd24a6b84b1959274
=Arjie Cristobal (MSI) <ajie.io@yahoo.com>
2025-08-03 17:39:56 -0400

    NextJS Application

--- turborepo/apps/web/app/page.tsx
+++ turborepo/apps/web/app/page.tsx
@@ -1,3 +1,14 @@
+import CreateProduct from "./create-product/create-product";
+import Products from "./products/products";
+
+export const dynamic = "force-dynamic";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <CreateProduct />
+      <Products />
+    </>
+  )
 }
+
